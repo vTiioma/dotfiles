5c003964830bb2290a255b551f5d6bb30fa09e35
vTiioma <artemsilantyev@gmail.com>
2024-09-01 14:33:35 -0400

    Create conform.lua

---
+++ .config/nvim/plugins/conform.lua
@@ -0,0 +1,42 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufWritePost", "BufNewFile" },
+	config = true,
+	keys = {
+		{
+			"F",
+			mode = { "n", "v" },
+			function()
+				require("conform").format({
+					lsp_fallback = true,
+					async = false,
+					timeout_ms = 500,
+				})
+			end,
+			desc = "[F]ormat file",
+		},
+	},
+	opts = {
+		stop_after_first = true,
+		formatters_by_ft = {
+			javascript = { "prettier", "prettierd" },
+			typescript = { "prettier", "prettierd" },
+			javascriptreact = { "prettier", "prettierd" },
+			typescriptreact = { "prettier", "prettierd" },
+			vue = { "prettier", "prettierd" },
+			css = { "prettier", "prettierd" },
+			scss = { "prettier", "prettierd" },
+			html = { "prettier", "prettierd" },
+			json = { "prettier", "prettierd" },
+			yaml = { "prettier", "prettierd" },
+			markdown = { "prettier", "prettierd" },
+			graphql = { "prettier", "prettierd" },
+			lua = { "stylua" },
+		},
+		-- format_on_save = {
+		-- 	lsp_fallback = true,
+		-- 	async = false,
+		-- 	timeout_ms = 500,
+		-- },
+	},
+}
