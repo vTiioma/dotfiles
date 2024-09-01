558e5a6a778f00fd09025f34eb932178c2e79721
vTiioma <artemsilantyev@gmail.com>
2024-09-01 14:28:50 -0400

    Create nvim ibl config file

---
+++ .config/nvim/plugins/ibl.lua
@@ -0,0 +1,12 @@
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  config = true,
+  opts = {
+    exclude = {
+      filetypes = {
+        "dashboard",
+      },
+    },
+  },
+}
