78f5b70412c3a5def4fa18f4fcf003e0a825ea8e
vTiioma <artemsilantyev@gmail.com>
2025-04-28 15:07:19 -0400

    Update oil.lua
    add ctrl+b keymap

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,7 +1,11 @@
 return {
   'stevearc/oil.nvim',
+	lazy = false,
   -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" },
+	keys = {
+		{ "<C-b>", "<CMD>Oil<CR>", desc = "Go to file explorer" }
+	},
   opts = {
     -- default_file_explorer = false,
     default_file_explorer = true,
